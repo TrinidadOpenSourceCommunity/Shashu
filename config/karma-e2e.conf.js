9d9a1b51935f7345550de6fc3cebca9f1ae2ef7f
ferron hanse <ferron@localhost.(none)>
2013-08-14 17:03:58 -0400

    lint all files and added jshint to grunt task

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -1,20 +1,20 @@
 // an example karma.conf.js
-module.exports = function(config) {
-  config.set({
-    basePath: '../',
-    frameworks: ['ng-scenario'],
-    files : [
-	  'test/e2e/**/*.js'
-	],
-	autoWatch : false,
-	singleRun : true,
-	browsers : ['Chrome'],
-	proxies : {
-  		'/': 'http://localhost:8000/'
-	},
-	junitReporter : {
-	  outputFile: 'test_out/e2e.xml',
-	  suite: 'e2e'
-	}
-  });
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+        frameworks: ['ng-scenario'],
+        files: [
+            'test/e2e/**/*.js'
+        ],
+        autoWatch: false,
+        singleRun: true,
+        browsers: ['Chrome'],
+        proxies: {
+            '/': 'http://localhost:8000/'
+        },
+        junitReporter: {
+            outputFile: 'test_out/e2e.xml',
+            suite: 'e2e'
+        }
+    });
 };
