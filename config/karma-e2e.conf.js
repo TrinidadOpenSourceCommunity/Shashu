411e6f42dcc6b380ed7dd75f2210456bfcc5d4ec
StevenChin <schin@medullan.com>
2013-09-08 22:12:57 -0400

    Modified config files to export a function

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -1,22 +1,26 @@
-basePath = '../';
-
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.js'
-];
-
-autoWatch = false;
-
-browsers = ['Chrome'];
-
-singleRun = true;
-
-proxies = {
-  '/': 'http://localhost:8000/'
-};
-
-junitReporter = {
-  outputFile: 'test_out/e2e.xml',
-  suite: 'e2e'
-};
+module.exports = function (config) {
+  config.set({
+		basePath : '../',
+		
+		files : [
+		  ANGULAR_SCENARIO,
+		  ANGULAR_SCENARIO_ADAPTER,
+		  'test/e2e/**/*.js'
+		],
+		
+		autoWatch : false,
+		
+		browsers : ['Chrome'],
+		
+		singleRun : true,
+		
+		proxies : {
+		  '/': 'http://localhost:8000/'
+		},
+		
+		junitReporter : {
+		  outputFile: 'test_out/e2e.xml',
+		  suite: 'e2e'
+		}
+	)}
+}
